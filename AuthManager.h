e42da277d3d5f162a61bb3320c18df54f30b01b4
nffdev <nightfrost2discord@outlook.fr>
2025-08-04 20:26:07 +0200

    feat(app): updated library

--- AuthManager.h
+++ AuthManager.h
@@ -14,10 +14,10 @@ void AuthManager_SetConfig(const char* appName, const char* ownerId, const char*
 bool AuthManager_CheckAppExists(const char* appName, const char* ownerId, const char* appSecret);
 bool AuthManager_CheckUserExists(const char* username, const char* password, const char* ownerId);
 bool AuthManager_CheckLicense(const char* license, const char* hwid, const char* ownerId);
-bool AuthManager_RegisterUser(const char* email, const char* username, const char* password, const char* license, const char* hwid, const char* ownerId);
+bool AuthManager_RegisterUser(const char* username, const char* password, const char* license, const char* hwid, const char* ownerId);
 
 // Utility functions
-bool AuthManager_ValidateInput(const char* email, const char* username, const char* password);
+bool AuthManager_ValidateInput(const char* username, const char* password);
 const char* AuthManager_GetHWID();
 
 // Authentication interface
